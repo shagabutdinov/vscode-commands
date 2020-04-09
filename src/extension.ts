18314904f9c6c32616f13e61229564303d566aeb
Leonid Shagabutdinov <leonid@shagabutdinov.com>
2020-04-09 17:05:13 +0300

    fix entrypoint

--- src/extension.ts
+++ src/extension.ts
@@ -4,26 +4,29 @@ import * as convert from "./extension/lib/convert";
 import * as commands from "./extension/commands";
 
 export function activate(context: vscode.ExtensionContext) {
-  const checkScope = (scope: any) =>
-    vscode.commands.executeCommand<boolean>("scope.check", scope);
+  const document = {
+    execute: vscode.commands.executeCommand,
+    checkScope: (scope: any) =>
+      vscode.commands.executeCommand<boolean>("scope.check", scope),
+    getSelections: () => vscode.window.activeTextEditor?.selections || [],
+    setSelections: (selections: vsc.Selection[]) => {
+      if (vscode.window.activeTextEditor) {
+        vscode.window.activeTextEditor.selections = convert.selections(
+          selections,
+        );
+      }
+    },
+  };
 
   context.subscriptions.push(
     vscode.commands.registerCommand("commands.run", (args: any) =>
-      commands.execute(
-        {
-          execute: vscode.commands.executeCommand,
-          checkScope,
-          getSelections: () => vscode.window.activeTextEditor?.selections || [],
-          setSelections: (selections: vsc.Selection[]) => {
-            if (vscode.window.activeTextEditor) {
-              vscode.window.activeTextEditor.selections = convert.selections(
-                selections,
-              );
-            }
-          },
-        },
-        args,
-      ),
+      commands.run(document, args),
+    ),
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("commands.execute", (args: any) =>
+      commands.execute(document, args),
     ),
   );
 }
